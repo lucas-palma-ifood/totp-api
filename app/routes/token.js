34a50149a44f6bb4c758a1eac3b8623ac285cf54
Lucas Palma <lucas.palma@ifood.com.br>
2020-10-30 15:50:05 -0300

    Set offset window for token validation

--- app/routes/token.js
+++ app/routes/token.js
@@ -3,7 +3,23 @@ var totp = require('otplib').totp;
 var router = express.Router();
 
 router.get('/:seed', function(req, res, next) {
+  totp.resetOptions();
   res.send(totp.generate(req.params.seed));
+});
+
+router.post('/', function(req, res, next) {
+  var offset = req.body.offset;
+  var token = req.body.token;
+  var seed = req.body.seed;
+
+  if (Number.isInteger(offset) && offset >= 0) {
+    totp.options = { window: offset };
+  } else {
+    res.status(400);
+    res.send('Offset value (' + offset + ') is invalid');
+  }
+
+  res.send(totp.check(token, seed));
 });
 
 module.exports = router;
